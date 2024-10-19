79f5933bd1a91e08853186dbf1148b436fb4edc2
Azadeh Ahmadi <azadehahmadi223@gmail.com>
2024-10-19 02:16:11 -0700

    Add a new message

--- src/server2.js
+++ src/server2.js
@@ -35,12 +35,22 @@ console.log("DB_PORT:", process.env.DB_PORT);
 
 
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: parseInt(process.env.DB_PORT) || 5432,
+// });
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT) || 5432,
+  ssl: {
+    rejectUnauthorized: false, // Set to true in production if you have valid certificates
+  },
 });
 
 
